fdcf9ea64abffeccfec7db946ee8beeafb386fdb
IngaKirsona <ingas@inbox.lv>
2020-08-22 20:32:17 +0300

    Initial Commit

---
+++ UIElementsLayoutHomework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIElementsLayoutHomework
+//
+//  Created by Inga Kirsona on 22/08/2020.
+//  Copyright © 2020 Inga Kirsona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
